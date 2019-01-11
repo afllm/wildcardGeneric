18ae4d61dccb3d2725aa5a865c9c3fa20dbbe753
a007243984l <a007243984l@s09-pc23.ausias.lan>
2019-01-11 17:44:47 +0100

    subida imagenes arreglada 11-01-2019

--- src/main/java/net/daw/service/specificServiceImplementation_1/ProductoService_1.java
+++ src/main/java/net/daw/service/specificServiceImplementation_1/ProductoService_1.java
@@ -1,5 +1,10 @@
 package net.daw.service.specificServiceImplementation_1;
 
+import net.daw.bean.beanImplementation.ReplyBean;
+import net.daw.bean.beanImplementation.ProductoBean;
+import net.daw.dao.specificDaoImplementation_1.ProductoDao_1;
+import net.daw.service.genericServiceImplementation.GenericServiceImplementation;
+import net.daw.service.publicServiceInterface.ServiceInterface;
 import com.google.gson.Gson;
 import java.io.File;
 import java.sql.Connection;
@@ -7,15 +12,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import net.daw.bean.beanImplementation.ReplyBean;
-import net.daw.bean.beanImplementation.ProductoBean;
 import net.daw.connection.publicinterface.ConnectionInterface;
 import net.daw.constant.ConnectionConstants;
-import net.daw.dao.specificDaoImplementation_1.ProductoDao_1;
 import net.daw.factory.ConnectionFactory;
 import net.daw.helper.EncodingHelper;
-import net.daw.service.genericServiceImplementation.GenericServiceImplementation;
-import net.daw.service.publicServiceInterface.ServiceInterface;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -27,6 +27,7 @@ public class ProductoService_1 extends GenericServiceImplementation implements S
 
     public ProductoService_1(HttpServletRequest oRequest) {
         super(oRequest);
+        this.oRequest = oRequest;
         ob = oRequest.getParameter("ob");
     }
 
